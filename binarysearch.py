48d3dc052fc6bfe65b5403cb897682d0960eff71
Hari Prapan <prapanhari01@gmail.com>
2023-10-09 20:42:04 +0530

    Create binarysearch.py

---
+++ binarysearch.py
@@ -0,0 +1,19 @@
+def binary_search(L, v):
+  if len(L) == 0:
+    return False
+  mid = len(L) // 2
+  if v == L[mid]:
+    return True
+  elif v < L[mid]:
+    return binary_search(L[:mid], v)
+  else:
+    return binary_search(L[mid + 1:], v)
+
+
+m = []
+for i in range(1000000):
+  m.append(i)
+
+print("List made")
+
+print(binary_search(m, 55555555555555))
